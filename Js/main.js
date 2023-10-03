76f707fea28a71d8e9a45590c3aea2f0a9301735
adiiaot <adiiaot11@gmail.com>
2023-10-03 13:07:01 +0100

    initialized git

---
+++ Js/main.js
@@ -0,0 +1,40 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+}
+
+// Notification 
+let bell = document.querySelector('.notification');
+
+document.querySelector('#bell-icon').onclick = () =>{
+    bell.classList.toggle('active');
+}
+
+
+
+//swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickabke: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 15,
+        },
+        1068: {
+            slidesPerView: 5,
+            spaceBetween: 20,
+        },
+    },
+});
